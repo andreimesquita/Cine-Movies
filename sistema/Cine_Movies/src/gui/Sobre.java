cac5fbbd6370162336c57ae8d40ce965fb93a4fa
andreirs <andreirs@outlook.com>
2013-09-13 08:30:14 -0300

    Atualização -> Casos de Uso

--- sistema/Cine_Movies/src/gui/Sobre.java
+++ sistema/Cine_Movies/src/gui/Sobre.java
@@ -1,6 +1,6 @@
 package gui;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -12,11 +12,15 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
-import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
 
 public class Sobre extends JDialog {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private ImageIcon icone;
 	private JLabel lblNomePrograma;
 	private JLabel lblicone;
@@ -45,7 +49,8 @@ public class Sobre extends JDialog {
 	private void initAtributos() {
 		icone = new ImageIcon("Imagens\\icone.gif");
 		lblicone = new JLabel(icone);
-		lblNomePrograma = new JLabel("Cine-Movies v0.0.1   (BETA)");
+		lblicone.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNomePrograma = new JLabel("Cine-Movies v0.0.1             (BETA)");
 		lblNomePrograma.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNomePrograma.setLabelFor(lblicone);
 		painelNome = new JPanel();
@@ -57,25 +62,36 @@ public class Sobre extends JDialog {
 		painelTexto = new JPanel();
 		painelTexto.setLayout(new BorderLayout(0, 0));
 		texto = new JTextPane();
+		texto.setEditable(false);
+		texto.setText("Este programa é um software livre; Você pode redistribuí-lo e/ou modificá-lo;\n"
+            + "Este programa é distribuído na esperança de que seja útil, mas SEM NENHUMA GARANTIA;");
 		painelTexto.add(texto);
-		btnOK = new JButton("OK");
-		btnOK.addActionListener(new OKAction());
 	}
 	
 	private void addAtributos() {
 		getContentPane().add(painelNome);
 		getContentPane().add(lblautor);
 		getContentPane().add(painelTexto);
+		
+		btnOK = new JButton("OK");
+		btnOK.addActionListener(new OKAction());
+		getContentPane().add(btnOK);
 	}
 	
 	private class OKAction extends AbstractAction {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
 		public OKAction() {
 			super("OK");
 		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			Sobre.this.dispose();
+			
+			System.out.println("XX");
 		}
 	}
 }
